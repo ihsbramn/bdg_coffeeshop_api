745c3ef2dfb2d5b87f0b4460e82d3c7f7e53a8a8
ihsbramn <abdurahmanihsan25@gmail.com>
2022-11-23 15:58:21 +0700

    db

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     multipleStatements: true,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'bdg_coffeeshop_db'
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'b4593dd73d111e',
+    password: '1348f0de',
+    database: 'heroku_35eba44dd94e75d'
 };
